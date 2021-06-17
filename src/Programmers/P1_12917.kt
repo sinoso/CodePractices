979b842a7fa169484ddc42e80971d2cb756b8899
sinoso <tpgus123123@naver.com>
2021-06-17 21:46:49 +0900

    P1_12917

---
+++ src/Programmers/P1_12917.kt
@@ -0,0 +1,9 @@
+package Programmers
+fun main() {
+    System.out.println(solution("Zbcdefg"))
+}
+private fun solution(s: String): String {
+    val a = s.toCharArray()
+            a.sortDescending()
+    return String(a)
+}
--- src/Programmers/test1.kt
+++ src/Programmers/test1.kt
@@ -1,15 +1,9 @@
 package Programmers
 
 fun main() {
-    var result: Int = 0
-    var rest: Int = 0
-    val array = arrayListOf<Int>()
-    var multi = 1
-    println(array)
-    for (a in  array.reversed()) {
-        println(a)
-        result += multi * a
-        multi *= 3
-    }
-    println("result$result")
+
+}
+
+private fun solution(): Int {
+    return 1
 }
