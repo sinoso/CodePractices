aa2d8b3538b2180ac2663719a4465769b13a965f
sinoso <tpgus123123@naver.com>
2021-06-18 23:23:13 +0900

    Create P1_12954.java

---
+++ src/Programmers/P1_12954.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class P1_12954 {
+    public static void main(String[] args) {
+        long[] solution = solution(1, 1);
+        for (long l : solution)
+            System.out.println(l);
+    }
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        int i = 0;
+        while(i<n){
+            answer[i] = x*(1+i++);
+        }
+        return answer;
+    }
+}
