8b071dbd30d04454d9928f125a41352b1c9af4ec
sinoso <tpgus123123@naver.com>
2021-06-28 01:27:35 +0900

    Create P1_12933.java

---
+++ src/Programmers/P1/P1_12933.java
@@ -0,0 +1,29 @@
+package Programmers.P1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class P1_12933 {
+    public static void main(String[] args) {
+        System.out.println(solution(118372));
+    }
+
+    public static long solution(long n) {
+        List<Long> list = new ArrayList<>();
+        long temp;
+        while(n>0){
+            temp = n%10;
+            list.add(temp);
+            n = n/10;
+        }
+        Collections.sort(list);
+        int size = list.size();
+        temp = 0L;
+        for(int i = size-1;i>=0;i--){
+            temp*=10;
+            temp+=list.get(i);
+        }
+        return temp;
+    }
+}
