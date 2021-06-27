799825766f2c61d82017a0e8c29c42747b05d2f0
sinoso <tpgus123123@naver.com>
2021-06-27 21:57:05 +0900

    Create P1_12944.java

---
+++ src/Programmers/P1/P1_12944.java
@@ -0,0 +1,12 @@
+package Programmers.P1;
+import java.util.Arrays;
+
+public class P1_12944 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1,2,3,4}));
+    }
+
+    public static double solution(int[] arr) {
+        return Arrays.stream(arr).reduce((a,b)->a+b).getAsInt()/(double)arr.length;
+    }
+}
