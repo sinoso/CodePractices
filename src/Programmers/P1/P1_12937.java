b991aaef2ed90388b4b22be1fd61769055f5e534
sinoso <tpgus123123@naver.com>
2021-06-27 22:44:08 +0900

    Create P1_12937.java

---
+++ src/Programmers/P1/P1_12937.java
@@ -0,0 +1,14 @@
+package Programmers.P1;
+
+public class P1_12937 {
+    public static void main(String[] args) {
+            System.out.println(solution(3));
+    }
+
+    public static String solution(int num) {
+        if (num % 2 == 0)
+            return "Even";
+        else
+            return "Odd";
+    }
+}
