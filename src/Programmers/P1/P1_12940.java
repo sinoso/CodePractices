3e7386db064717f86c0e4af0a44fd9cb6d1eda29
sinoso <tpgus123123@naver.com>
2021-06-28 23:31:27 +0900

    Create P1_12940.java

---
+++ src/Programmers/P1/P1_12940.java
@@ -0,0 +1,36 @@
+package Programmers.P1;
+
+import java.util.Arrays;
+
+public class P1_12940 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(6, 8)));
+    }
+
+    public static int[] solution(int n, int m) {
+        return new int[]{gcd(n, m), lcm(n, m)};
+    }
+
+    public static int lcm(int n, int m) {
+        int min = Math.min(n, m);
+        int max = Math.max(n, m);
+        int lcm = 1;
+        for (int i = 2; i <= min; ) {
+            if (min % i == 0 && max % i == 0) {
+                min /= i;
+                max /= i;
+                lcm *= i;
+            } else
+                i++;
+        }
+        lcm = lcm * min * max;
+        return lcm;
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+}
