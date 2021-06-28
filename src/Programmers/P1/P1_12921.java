cff6ecd111c9cc021469f870302b2869fd09cd87
sinoso <tpgus123123@naver.com>
2021-06-28 18:24:07 +0900

    Create P1_12921.java

---
+++ src/Programmers/P1/P1_12921.java
@@ -0,0 +1,44 @@
+package Programmers.P1;
+
+import java.util.*;
+
+public class P1_12921 {
+    public static void main(String[] args) {
+//        System.out.println(solution(10));
+//        System.out.println(solution(100000));
+        System.out.println(solution(2));
+    }
+    public static int solution(int n) {
+        int[] fullSized = new int[n + 1];
+        int result = 0;
+        for (int i = 2; i <= n; i++) {
+            fullSized[i] = i;
+        }
+        for (int i = 2; i <= n; i++) {
+            if (fullSized[i] == 0)
+                continue;
+            result++;
+            for (int j = i + i; j <= n; j += i) {
+                fullSized[j] = 0;
+            }
+        }
+        return result;
+    }
+    public static int noSolution1(int n) {
+        int answer = 1;
+        if(n==2)
+            return answer;
+        for (int i = 3; i <= n; i++) {
+            boolean flag = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+                answer++;
+        }
+        return answer;
+    }
+}
