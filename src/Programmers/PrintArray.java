b20c6c2994f1b1a860ee7a3e9b9db061485d931f
sinoso <tpgus123123@naver.com>
2021-06-28 22:55:39 +0900

    Update PrintArray.java

--- src/Programmers/PrintArray.java
+++ src/Programmers/PrintArray.java
@@ -1,25 +1,28 @@
 package Programmers;
 
 public class PrintArray {
-    public static void printArray(int[][] array){
+    public static void printArray(int[][] array) {
         StringBuilder sb = new StringBuilder();
-        for(int[] subArray :array){
-            for(int value:subArray){
+        for (int[] subArray : array) {
+            for (int value : subArray) {
                 sb.append(String.format("%3d  ", value));
             }
             sb.append("\n");
         }
         System.out.println(sb);
-    }    public static void printArray2(int[][] array){
+    }
+
     public static void printArray2(int[][] array) {
         StringBuilder sb = new StringBuilder();
-        for(int[] subArray :array){
+        for (int[] subArray : array) {
             sb.append("[");
-            for(int value:subArray){
+            for (int value : subArray) {
                 sb.append(String.format("%3d ", value));
             }
             sb.append("]\n");
         }
         System.out.println(sb);
     }
+
+
 }
