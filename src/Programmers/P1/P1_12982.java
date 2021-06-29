632e963fbfd6abf4c30aadf6daa3302d04c1efaa
sinoso <tpgus123123@naver.com>
2021-06-29 17:00:11 +0900

    Create P1_12982.java

---
+++ src/Programmers/P1/P1_12982.java
@@ -0,0 +1,23 @@
+package Programmers.P1;
+
+import java.util.Arrays;
+
+public class P1_12982 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 3, 2, 5, 4}, 9));//3
+        System.out.println(solution(new int[]{2, 2, 3, 3}, 10));//4
+    }
+
+    public static int solution(int[] d, int budget) {
+        int used = 0;
+        int answer = 0;
+        d = Arrays.stream(d).sorted().toArray();
+        for (int requested : d) {
+            if (requested + used > budget)
+                break;
+            used += requested;
+            answer++;
+        }
+        return answer;
+    }
+}
