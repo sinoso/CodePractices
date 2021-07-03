2fd7c0af4baf6bbab85972634d262cf1ab18e596
sinoso <tpgus123123@naver.com>
2021-07-03 18:27:25 +0900

    Create P1_42840.kt

---
+++ src/Programmers/P1/P1_42840.kt
@@ -0,0 +1,19 @@
+package Programmers
+
+fun main() {
+    println(solution(intArrayOf(1, 2, 3, 4, 5)).contentToString())
+    println(solution(intArrayOf(1, 3, 2, 4, 2)).contentToString())
+}
+
+private fun solution(answers: IntArray): IntArray {
+    var answer = IntArray(3)
+    val supojas = arrayOf(intArrayOf(1, 2, 3, 4, 5), intArrayOf(2, 1, 2, 3, 2, 4, 2, 5), intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5))
+    for (i in answers.indices) {
+        for (j in supojas.indices) {
+            if (answers[i] == supojas[j][i % supojas[j].size]) {
+                answer[j]++
+            }
+        }
+    }
+    return supojas.indices.filter { v -> answer[v] == answer.max() }.map { v -> v + 1 }.toIntArray()
+}
