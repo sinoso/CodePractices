fe43a39ad4cf106c8825b869cb5477714c543287
sinoso <tpgus123123@naver.com>
2021-07-04 16:15:01 +0900

    Create P2_12939.kt

---
+++ src/Programmers/P2/P2_12939.kt
@@ -0,0 +1,14 @@
+package Programmers.P2
+
+fun main() {
+    println(solution("1 2 3 4"))
+    println(solution("-1 -2 -3 -4"))
+    println(solution("-1 -1"))
+}
+
+private fun solution(s: String): String = s.split(" ").map { it.toInt() }.let { "${it.min()} ${it.max()}" }
+
+private fun solution2(s: String): String {
+    var intArray = s.split(" ").map { v -> v.toInt() }
+    return "${intArray.min()} ${intArray.max()}"
+}
