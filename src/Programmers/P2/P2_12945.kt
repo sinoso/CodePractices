4925ea79378989af70f6b687d31a5a15d258b29c
sinoso <tpgus123123@naver.com>
2021-07-04 21:20:51 +0900

    Create P2_12945.kt

---
+++ src/Programmers/P2/P2_12945.kt
@@ -0,0 +1,32 @@
+package Programmers.P2
+
+fun main() {
+    println(solution(3))
+    println(solution(5))
+    println(solution(100000))
+}
+
+private fun solution(n: Int): Int {
+    var beforeLastValue = 0
+    var lastValue = 1
+    var temp = 0
+    if (n == 1)
+        return 1
+    for (i in 0..(n - 2)) {
+        temp = (beforeLastValue + lastValue) % 1234567
+        beforeLastValue = lastValue
+        lastValue = temp
+    }
+    return lastValue
+}
+
+private fun solution2(n: Int): Int {
+    var answer = 0
+    var fibonacci: MutableList<Int> = ArrayList()
+    fibonacci.add(0)
+    fibonacci.add(1)
+    for (i in 0..(n - 2)) {
+        fibonacci.add((fibonacci[fibonacci.lastIndex] + fibonacci[fibonacci.lastIndex - 1]) % 1234567)
+    }
+    return fibonacci.last()
+}
