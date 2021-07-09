090fccab74aeafd9c561d449004a38835857f12e
sinoso <tpgus123123@naver.com>
2021-07-09 23:34:35 +0900

    Create P1_12912.kt

---
+++ src/Programmers/P1/P1_12912.kt
@@ -0,0 +1,17 @@
+package Programmers
+
+
+fun main() {
+    println(solution(3, 5))
+}
+
+private fun solution(a: Int, b: Int): Long {
+    var answer: Long
+    val max = a.coerceAtLeast(b)
+    val min = a.coerceAtMost(b)
+    answer = min.toLong()
+    for (i in 1 until max - min + 1) {
+        answer += min + i.toLong()
+    }
+    return answer
+}
