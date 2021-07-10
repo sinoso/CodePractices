3379eb5dd225028df13ffda068ba7324531c33c9
sinoso <tpgus123123@naver.com>
2021-07-10 13:32:30 +0900

    Create P1_42748.kt

---
+++ src/Programmers/P1/P1_42748.kt
@@ -0,0 +1,7 @@
+package Programmers
+
+fun main() {
+    println(solution(intArrayOf(1, 5, 2, 6, 3, 7, 4), arrayOf(intArrayOf(2, 5, 3), intArrayOf(4, 4, 1), intArrayOf(1, 7, 3))).contentToString())
+}
+
+private fun solution(array: IntArray, commands: Array<IntArray>): IntArray = commands.map { command -> array.clone().copyOfRange(command[0] - 1, command[1]).sorted()[command[2] - 1] }.toIntArray()
