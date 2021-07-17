abfef28771e87ac24cc580cb2043ca343539c561
sinoso <tpgus123123@naver.com>
2021-07-18 01:50:24 +0900

    Create P1_12932.kt

---
+++ src/Programmers/P1/P1_12932.kt
@@ -0,0 +1,10 @@
+package Programmers
+
+import kotlin.math.*
+
+fun main() {
+    println(solution(1234).joinToString())
+    println(solution(3).joinToString())
+}
+
+private fun solution(n: Long): IntArray = n.toString().reversed().map { v -> Character.getNumericValue(v) }.toIntArray()
