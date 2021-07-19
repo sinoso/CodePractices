148863e3799ed36d0073dc6effae7f7e71403841
sinoso <tpgus123123@naver.com>
2021-07-19 14:41:18 +0900

    Create P1_12933.kt

---
+++ src/Programmers/P1/P1_12933.kt
@@ -0,0 +1,10 @@
+package Programmers
+
+
+fun main() {
+    println(solution(118372))
+    println(solution(80000001000))
+}
+
+private fun solution(n: Long): Long = n.toString().map { v -> Character.getNumericValue(v) }.sortedDescending().joinToString("").toLong()
+private fun solution2(n: Long): Long = String(n.toString().toCharArray().sortedArrayDescending()).toLong()
