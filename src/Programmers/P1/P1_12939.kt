08da42465b2aa0880ddb3334915f77242730dc81
sinoso <tpgus123123@naver.com>
2021-07-19 14:15:18 +0900

    Create P1_12939.kt

---
+++ src/Programmers/P1/P1_12939.kt
@@ -0,0 +1,10 @@
+package Programmers
+fun main() {
+    println(solution("1 2 3 4"))
+    println(solution("-1 -2 -3 -4"))
+}
+
+private fun solution(s: String): String {
+    val intArray = s.split(" ").map { v -> v.toInt() }
+    return "${intArray.min()} ${intArray.max()}"
+}
