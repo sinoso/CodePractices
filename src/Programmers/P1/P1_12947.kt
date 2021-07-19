b2c736bedcd74ac9de3965c3f1cb9d6b09401450
sinoso <tpgus123123@naver.com>
2021-07-19 13:29:55 +0900

    Create P1_12947.kt

---
+++ src/Programmers/P1/P1_12947.kt
@@ -0,0 +1,11 @@
+package Programmers
+
+fun main() {
+    println(solution(10))
+    println(solution2(12))
+    println(solution(11))
+    println(solution2(13))
+}
+
+private fun solution(x: Int): Boolean = x % x.toString().map { v -> Character.getNumericValue(v) }.reduce { a, b -> a + b } == 0
+private fun solution2(x: Int): Boolean = x % x.toString().map { v -> Character.getNumericValue(v) }.sum() == 0
