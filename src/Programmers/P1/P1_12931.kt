2de9c99d72c47db43c87b6163ab7e344a37cad7c
sinoso <tpgus123123@naver.com>
2021-07-19 14:29:49 +0900

    Create P1_12931.kt

---
+++ src/Programmers/P1/P1_12931.kt
@@ -0,0 +1,8 @@
+package Programmers
+
+fun main() {
+    println(solution(123))
+    println(solution(100000000))
+}
+
+private fun solution(n: Int): Int = n.toString().sumBy { v -> Character.getNumericValue(v) }
