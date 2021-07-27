c5161406c0a502c0b381527676003d1e92c3c199
sinoso <tpgus123123@naver.com>
2021-07-28 02:18:26 +0900

    Create P2_68645.kt

---
+++ src/Programmers/P2/P2_68645.kt
@@ -0,0 +1,46 @@
+package Programmers.P2
+
+
+fun main() {
+    println(solution(4).contentToString())
+//    println(solution(5).contentToString())
+//    println(solution(6).contentToString())
+}
+
+private fun solution(n: Int): IntArray {
+    var answer = IntArray((1 + n) * n / 2)
+    var lastIndex = 0
+    var lastValue = 1
+    var tries = 0
+    var lastLine = 0
+
+    while (tries < n) {
+        when (tries % 3) {
+            0 -> { //상 -> 하
+                while (lastLine < n && answer[lastIndex + lastLine] == 0) {
+                    lastIndex += lastLine
+                    answer[lastIndex] = lastValue++
+                    lastLine++
+                    println(answer.contentToString())
+                }
+            }
+            1 -> { //좌 -> 우
+                while (lastIndex < answer.lastIndex && answer[lastIndex + 1] == 0) {
+                    lastIndex += 1
+                    answer[lastIndex] = lastValue++
+                    println(answer.contentToString())
+                }
+            }
+            2 -> { //하 -> 상
+                while (answer[lastIndex - lastLine] == 0) {
+                    lastIndex -= lastLine
+                    answer[lastIndex] = lastValue++
+                    lastLine--
+                    println(answer.contentToString())
+                }
+            }
+        }
+        tries++
+    }
+    return answer
+}
