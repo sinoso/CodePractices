f9edc18eb02ddcb57746d529aa273648407af96f
sinoso <tpgus123123@naver.com>
2021-07-30 17:15:29 +0900

    Update P3_12927.java

--- src/Programmers/P3/P3_12927.java
+++ src/Programmers/P3/P3_12927.java
@@ -2,11 +2,12 @@ package Programmers.P3;
 
 import java.util.Arrays;
 
-public class P3_12927_B {
+public class P3_12927 {
     public static void main(String[] args) {
         System.out.println(solution(5, new int[]{4, 3, 3}));
         System.out.println(solution(5, new int[]{4}));
         System.out.println(solution(12, new int[]{4, 5}));
+        System.out.println(solution(0, new int[]{4, 5}));
     }
 
     public static long solution(int n, int[] works) {
@@ -37,30 +38,7 @@ public class P3_12927_B {
                 index--;
             left--;
         }
-        return Arrays.stream(works).map(v -> v * v).reduce(Integer::sum).orElse(0);
-    }
-
-    public static long solution2(int n, int[] works) {
-        long answer = 0;
-        Arrays.sort(works);
-        int lastIndex = works.length - 1;
-        int index = lastIndex;
-        int left = n;
-        while (left != 0) {
-            if (works[index - 1] < works[index]) {// index직전의 숫자보다 지금이 크다면?
-                left -= works[index] - works[index - 1];
-                works[index] = works[index - 1];
-            } else if (works[index - 1] == works[index]) {
-                if (index == lastIndex) {
-
-                }
-            }
-        }
-        //소팅한다
-        //뒤에서부터 1씩뺀다 이때 앞의 숫자보다 적어야한다.
-        //이때 앞의 숫자랑 현재뺀숫자가 같아지면 ?
-        // 맨뒤에서 부터 다시 시작!
-        return answer;
+        return Arrays.stream(works).mapToLong(v -> (long)v * v).reduce(Long::sum).getAsLong();
     }
 
 }
