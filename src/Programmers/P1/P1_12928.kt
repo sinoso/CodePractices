db333ad1849300458ae474d743167cb5a8a1d083
sinoso <tpgus123123@naver.com>
2021-07-31 18:16:57 +0900

    Create P1_12928.kt

---
+++ src/Programmers/P1/P1_12928.kt
@@ -0,0 +1,12 @@
+package Programmers
+
+fun main() {
+    println(solution(0))
+    println(solution(1))
+    println(solution(2))
+    println(solution(5))
+    println(solution(12))
+}
+
+private fun solution(n: Int): Int = (1..n).filter {n % it == 0 }.sum()
+private fun solution2(n: Int): Int = (1..n).filter { v -> n % v == 0 }.foldRight(0) { total, v -> total + v }
