396f443a3c570150d8ae84760040fd87c025cdce
sinoso <tpgus123123@naver.com>
2021-09-17 01:40:13 +0900

    Create lm_189.kt

---
+++ src/leetcode/medium/lm_189.kt
@@ -0,0 +1,20 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+//    println(intArrayOf(1, 2, 3, 4, 5, 6, 7).also { rotate(it, 3) }.joinToString())
+    println(intArrayOf(-1, -100, 3, 99).also { rotate(it, 2) }.joinToString())
+//    println(intArrayOf(1,2).also { rotate(it, 3) }.joinToString())
+//    println(intArrayOf(1, 2, 3).also { rotate(it, 4) }.joinToString())
+
+
+}
+
+fun rotate(nums: IntArray, k: Int): Unit {
+    val point = nums.size - k % nums.size
+    val temp = nums.slice(0 until point)
+    var point2 = 0
+    for (i in point..nums.lastIndex)
+        nums[point2++] = nums[i]
+    for (i in temp)
+        nums[point2++] = i
+}
