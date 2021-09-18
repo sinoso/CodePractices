5f48237d69c5f691404fe242f434b3ed742d3d98
sinoso <tpgus123123@naver.com>
2021-09-18 15:39:12 +0900

    Create le_167.kt

---
+++ src/leetcode/easy/le_167.kt
@@ -0,0 +1,40 @@
+package leetcode.easy
+
+fun main() {
+    println(twoSum(intArrayOf(2, 7, 11, 15), 9).contentToString())
+}
+
+
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+    var first = 0
+    var second = numbers.lastIndex
+    while (first != second) {
+        when {
+            numbers[first] + numbers[second] == target ->
+                return intArrayOf(first+1,second+1)
+            numbers[first] + numbers[second] > target ->
+                second--
+            numbers[first] + numbers[second] < target ->
+                first++
+        }
+    }
+    return intArrayOf(first+1,second+1)
+}
+
+fun twoSum2(numbers: IntArray, target: Int): IntArray {
+    var first = 0
+    var second = 0
+    for (i in numbers.indices) {
+        val target2 = target - numbers[i]
+        first = i
+        for (j in (i + 1)..numbers.lastIndex) {
+            if (second != 0)
+                break
+            if (numbers[j] == target2)
+                second = j
+        }
+        if (second != 0)
+            break
+    }
+    return intArrayOf(first + 1, second + 1)
+}
