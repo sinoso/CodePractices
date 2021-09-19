c0c579deb7466a4754259508f2c1e5344beee532
sinoso <tpgus123123@naver.com>
2021-09-20 00:24:46 +0900

    Create le_876.kt

---
+++ src/leetcode/easy/le_876.kt
@@ -0,0 +1,22 @@
+package leetcode.easy
+
+fun main() {
+    println(middleNode(ListNode(1).also { it1 -> it1.next = ListNode(2).also { it2 -> it2.next = ListNode(3).also { it3 -> it3.next = ListNode(4) } } })?.i)
+    println(middleNode(ListNode(1).also { it1 -> it1.next = ListNode(2).also { it2 -> it2.next = ListNode(3).also { it3 -> it3.next = ListNode(4).also { it4 -> it4.next = ListNode(5) } } } })?.i)
+    println(middleNode(ListNode(1).also { it1 -> it1.next = ListNode(2).also { it2 -> it2.next = ListNode(3).also { it3 -> it3.next = ListNode(4).also { it4 -> it4.next = ListNode(5) } } } })?.i)
+    println(middleNode(ListNode(1))?.i)
+}
+
+class ListNode(val i:Int) {
+    var next: ListNode? = null
+}
+
+private fun middleNode(head: ListNode?): ListNode? {
+    var left = head
+    var right = head
+    while (right != null) {
+        right = right.next?.also { left = left?.next }
+        right = right?.next
+    }
+    return left
+}
