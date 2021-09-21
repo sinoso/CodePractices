b093b6e134379eb93c4ed231082e10d5ee866e9c
sinoso <tpgus123123@naver.com>
2021-09-21 21:18:04 +0900

    Create lm_19.kt

---
+++ src/leetcode/medium/lm_19.kt
@@ -0,0 +1,63 @@
+package leetcode.medium
+
+fun main() {
+    fun ListNode.printAllN() {
+        var next: ListNode? = this
+        while (next != null) {
+            print("${next.i} ")
+            next = next.next
+        }
+        println()
+    }
+    removeNthFromEnd(ListNode(1), 1)?.printAllN()
+    println()
+    removeNthFromEnd(ListNode(1).also { it1 -> it1.next = ListNode(2) }, 2)?.printAllN()
+    removeNthFromEnd(ListNode(1).also { it1 -> it1.next = ListNode(2) }, 1)?.printAllN()
+    removeNthFromEnd(ListNode(1).also { it1 -> it1.next = ListNode(2).also { it2 -> it2.next = ListNode(3) } }, 3)?.printAllN()
+    removeNthFromEnd(ListNode(1).also { it1 -> it1.next = ListNode(2).also { it2 -> it2.next = ListNode(3).also { it3 -> it3.next = ListNode(4) } } }, 4)?.printAllN()
+    removeNthFromEnd(ListNode(1).also { it1 -> it1.next = ListNode(2).also { it2 -> it2.next = ListNode(3).also { it3 -> it3.next = ListNode(4).also { it4 -> it4.next = ListNode(5) } } } }, 4)?.printAllN()
+    removeNthFromEnd(ListNode(1).also { it1 -> it1.next = ListNode(2).also { it2 -> it2.next = ListNode(3).also { it3 -> it3.next = ListNode(4).also { it4 -> it4.next = ListNode(5) } } } }, 2)?.printAllN()
+}
+
+class ListNode(val i: Int) {
+    var next: ListNode? = null
+}
+
+// 처음이면?
+//중간이면?
+//끝이면?
+fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    var left: ListNode? = head
+    var right: ListNode? = left?.next
+    if (right == null)
+        return null
+    else if (right.next == null)
+        if (n == 1)
+            head?.next = null
+        else
+            return head?.next
+    for (i in 1 until n)
+        right = right?.next
+    if (right == null)
+        return head?.next
+    while (right?.next!=null){
+        right = right.next
+        left = left?.next
+    }
+    left?.next = left?.next?.next
+    return head
+}
+//fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+//    var left: ListNode? = head
+//    var right: ListNode? = head
+//    for (i in 1..n)
+//        right = right?.next
+//    while (right != null) {
+//        right = right.next
+//        left = left?.next
+//    }
+//    if(head == left)
+//        return head?.next
+//    left?.next = left?.next?.next
+//    return head
+//}
