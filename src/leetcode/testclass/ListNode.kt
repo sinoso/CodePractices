9b70dfbdb3ad01670c14e13c44fa992b9493415d
sinoso <tpgus123123@naver.com>
2021-09-26 02:48:47 +0900

    Update ListNode.kt

--- src/leetcode/testclass/ListNode.kt
+++ src/leetcode/testclass/ListNode.kt
@@ -2,4 +2,13 @@ package leetcode.testclass
 
 class ListNode(val `val`:Int) {
     var next: ListNode? = null
+
+    fun printAllN() {
+        var next: ListNode? = this
+        while (next != null) {
+            print("${next.`val`} ")
+            next = next.next
+        }
+        println()
+    }
 }
