0e61aae512680d941d79c12aa2074ff201e24e7d
sinoso <tpgus123123@naver.com>
2021-09-29 22:35:35 +0900

    Create lm_2.kt

---
+++ src/leetcode/medium/lm_2.kt
@@ -0,0 +1,31 @@
+package leetcode.medium
+
+import leetcode.testclass.ListNode
+
+fun main() {
+//    addTwoNumbers(ListNode(9), ListNode(9).also { it1 -> it1.next = ListNode(2) })?.printAllN()
+//    addTwoNumbers(ListNode(1).also { it1 -> it1.next = ListNode(2) }, ListNode(1).also { it1 -> it1.next = ListNode(2) })?.printAllN()
+//    println()
+}
+//
+//private fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+//
+//}
+//
+//private fun rec(l1: ListNode?, l2: ListNode?, pre: ListNode?, left: Int): ListNode? {
+//    when {
+//        l1 == null && l2 == null && left == 0 -> {
+//            return null
+//        }
+//        l1 == null -> {
+//            return rec(l1?.next?:null)
+//        }
+//        l2 == null -> {
+//
+//        }
+//        else -> {
+//
+//        }
+//    }
+//    return pre
+//}
