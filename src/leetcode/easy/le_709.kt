e52ddaaefd9e1042fd8b6c3eb8c6237e0c0da354
sinoso <tpgus123123@naver.com>
2021-10-03 18:24:11 +0900

    Create le_709.kt

---
+++ src/leetcode/easy/le_709.kt
@@ -0,0 +1,11 @@
+package leetcode.easy
+
+fun main() {
+    println(toLowerCase("Hello"))//2
+    println(toLowerCase("here"))//2
+    println(toLowerCase("LOVELY"))//2
+
+
+}
+
+private fun toLowerCase(s: String): String  = s.toLowerCase()
