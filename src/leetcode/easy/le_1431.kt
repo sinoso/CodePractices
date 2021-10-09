6ef5465535addec22de361889ef8d794882f0e45
sinoso <tpgus123123@naver.com>
2021-10-09 18:26:02 +0900

    Create le_1431.kt

---
+++ src/leetcode/easy/le_1431.kt
@@ -0,0 +1,10 @@
+package leetcode.easy
+
+fun main() {
+    println(kidsWithCandies(intArrayOf(2, 3, 5, 1, 3), 3).joinToString())
+}
+
+private fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> =
+        (candies.max() ?: 0).run {
+            candies.map { it + extraCandies >= this }
+        }
