f4fdb153ce6ae67fe35fb2b0a4e3dffca8a43b86
sinoso <tpgus123123@naver.com>
2021-10-11 17:52:40 +0900

    Create le_1528.kt

---
+++ src/leetcode/easy/le_1528.kt
@@ -0,0 +1,9 @@
+package leetcode.easy
+
+fun main() {
+    println(restoreString("codeleet", intArrayOf(4, 5, 6, 7, 0, 2, 1, 3)))
+    println(restoreString("aiohn", intArrayOf(3, 1, 4, 2, 0)))
+}
+private fun restoreString(s: String, indices: IntArray): String =
+        s.withIndex().sortedBy { indices[it.index] }.map { it.value }.joinToString("")
+
