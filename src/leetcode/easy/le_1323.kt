9ec5b83d953336fbe5235183da6360d1dc2632b9
sinoso <tpgus123123@naver.com>
2021-12-06 00:01:34 +0900

    Update le_1323.kt

--- src/leetcode/easy/le_1323.kt
+++ src/leetcode/easy/le_1323.kt
@@ -9,9 +9,25 @@ fun main() {
 
 
 private fun maximum69Number(num: Int): Int {
+    val size = Math.log10(num.toDouble()).toInt()
+    var divider = Math.pow(10.0, size.toDouble()).toInt()
+    var rest = num
+    while (rest != 0) {
+        val temp: Int = rest / divider
+        if (temp == 6)
+            break
+        rest %= divider
+        divider = divider/ 10 
+    }
+    if (rest == 0)
+        return num
+    return num + divider * 3
+}
+
+private fun maximum69Number2(num: Int): Int {
     val array = num.toString().toCharArray()
     val index = array.indexOfFirst { it == '6' }
-    if(index==-1)
+    if (index == -1)
         return num
     array[index] = '9'
     return String(array).toInt()
