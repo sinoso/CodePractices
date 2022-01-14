a7b8cf061e70abf105f281c4420bf4fe3bf0c3ea
sinoso <tpgus123123@naver.com>
2022-01-15 03:23:19 +0900

    Create P1_92334.kt

---
+++ src/programmers/p1/P1_92334.kt
@@ -0,0 +1,31 @@
+package programmers
+
+fun main() {
+    println(
+        solution(
+            arrayOf("muzi", "frodo", "apeach", "neo"),
+            arrayOf("muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"),
+            2
+        ).joinToString()
+    )
+    println(
+        solution(
+            arrayOf("con", "ryan"),
+            arrayOf("ryan con", "ryan con", "ryan con", "ryan con"),
+            2
+        ).joinToString()
+    )
+}
+
+private fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray =
+    report.map { it.split(" ") }
+        .groupBy { it[1] }
+        .asSequence()
+        .map { it.value.distinct() }
+        .filter { it.size >= k }
+        .flatten()
+        .map { it[0] }
+        .groupingBy { it }
+        .eachCount()
+        .run { id_list.map { getOrDefault(it, 0) }.toIntArray() }
+
