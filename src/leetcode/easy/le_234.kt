b0f86d396e0cd8a8f848f2500148492b1d460a28
sinoso <tpgus123123@naver.com>
2022-01-16 02:16:03 +0900

    Update le_234.kt

--- src/leetcode/easy/le_234.kt
+++ src/leetcode/easy/le_234.kt
@@ -14,6 +14,21 @@ private fun isPalindrome(head: ListNode?): Boolean {
         list.add(current.`val`)
         current = current.next
     }
+    var left = 0
+    var right = list.lastIndex
+    while (left < right)
+        if (list[left++] != list[right--])
+            return false
+    return true
+}
+
+private fun isPalindrome2(head: ListNode?): Boolean {
+    val list = mutableListOf<Int>()
+    var current = head
+    while (current != null) {
+        list.add(current.`val`)
+        current = current.next
+    }
     var idx2 = list.lastIndex
     (0..(list.size - 1) / 2).forEach {
         if (list[it] != list[idx2--])
