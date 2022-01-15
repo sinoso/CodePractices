f44e56391a3a01ab8e10344f045b3992c54c07d8
sinoso <tpgus123123@naver.com>
2022-01-16 01:52:43 +0900

    Update P1_12903.kt

--- src/programmers/p1/P1_12903.kt
+++ src/programmers/p1/P1_12903.kt
@@ -3,12 +3,8 @@ package programmers
 fun main() {
     System.out.println(solution("abcde"))
 }
-private fun solution(s: String): String {
-    var answer = ""
-    var length = s.length
-    if (length % 2 == 0)
-        answer = s.substring(length / 2 - 1, length / 2 + 1)
-    else
-        answer = s.substring(length / 2, length / 2 + 1)
-    return answer
-}
+
+private fun solution(s: String): String = if (s.length % 2 == 0)
+    s.substring(s.length / 2 - 1, s.length / 2 + 1)
+else
+    s.substring(s.length / 2, s.length / 2 + 1)
