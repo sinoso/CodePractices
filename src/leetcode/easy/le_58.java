4af1f6520b272abd374eac77a8208ee20c8efc03
sinoso <tpgus123123@naver.com>
2022-01-25 23:04:49 +0900

    Create le_58.java

---
+++ src/leetcode/easy/le_58.java
@@ -0,0 +1,22 @@
+package leetcode.easy;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class le_58 {
+    public static void main(String... args) {
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+    }
+
+    private static int lengthOfLastWord(String s) {
+        return Arrays.stream(
+                new StringBuffer(s)
+                        .reverse()
+                        .toString()
+                        .split(" "))
+                .filter(it-> !it.equals(""))
+                .findFirst()
+                .get()
+                .length();
+    }
+}
