c5262f4e80710e46b9af0b8edca7c0fa035add68
sinoso <tpgus123123@naver.com>
2022-02-14 13:37:03 +0900

    Update le_104.kt

--- src/leetcode/easy/le_104.kt
+++ src/leetcode/easy/le_104.kt
@@ -14,7 +14,11 @@ fun main() {
     printResult(maxDepth(node1), 3)
 }
 
-private fun maxDepth(root: TreeNode?, depth: Int = 0): Int {
+private fun maxDepth(root: TreeNode?, depth: Int = 0): Int =
+    root?.let { maxDepth(it.left, depth + 1).coerceAtLeast(maxDepth(it.right, depth + 1)) } ?: depth
+
+
+private fun maxDepth2(root: TreeNode?, depth: Int = 0): Int {
     root ?: return depth
     val leftDepth = maxDepth(root.left, depth + 1)
     val rightDepth = maxDepth(root.right, depth + 1)
