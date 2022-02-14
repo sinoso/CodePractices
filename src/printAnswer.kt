e1c487d97b8d12800c67233ced14da84f6cdb680
sinoso <tpgus123123@naver.com>
2022-02-14 11:11:10 +0900

    Update printAnswer.kt

--- src/printAnswer.kt
+++ src/printAnswer.kt
@@ -1,4 +1,5 @@
 
 fun <T> printResult(actual: T, expected: T) =
-    println(actual.also { if (actual == expected) print("pass ") else print("fail ") })
+    println(actual.also { if (actual == expected) print("pass ") else print("fail ")
+    print("expected = $expected , actual = ")})
 
