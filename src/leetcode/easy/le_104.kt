92a3278e1e5004900092f6d4b8d875fe42736144
sinoso <tpgus123123@naver.com>
2022-02-14 11:11:41 +0900

    Create le_104.kt

---
+++ src/leetcode/easy/le_104.kt
@@ -0,0 +1,22 @@
+package leetcode.easy
+
+import leetcode.testclass.TreeNode
+import printResult
+
+fun main() {
+    val node1 = TreeNode(3).apply {
+        left = TreeNode(6)
+        right = TreeNode(20).apply {
+            left = TreeNode(15)
+            right = TreeNode(7)
+        }
+    }
+    printResult(maxDepth(node1), 3)
+}
+
+private fun maxDepth(root: TreeNode?, depth: Int = 0): Int {
+    root ?: return depth
+    val leftDepth = maxDepth(root.left, depth + 1)
+    val rightDepth = maxDepth(root.right, depth + 1)
+    return leftDepth.coerceAtLeast(rightDepth)
+}
