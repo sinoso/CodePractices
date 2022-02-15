35a15e0022032500a04857fbf0ec3d4842981500
sinoso <tpgus123123@naver.com>
2022-02-15 11:12:19 +0900

    Update le_136.kt

--- src/leetcode/easy/le_136.kt
+++ src/leetcode/easy/le_136.kt
@@ -7,5 +7,10 @@ fun main() {
 private fun singleNumber(nums: IntArray): Int =
     mutableMapOf<Int, Int>().apply {
         nums.forEach { num -> this[num] = this.getOrDefault(num, 0) + 1 }
+    }.entries.first { it.value == 1 }.key
+
+private fun singleNumber2(nums: IntArray): Int =
+    mutableMapOf<Int, Int>().apply {
+        nums.forEach { num -> this[num] = this.getOrDefault(num, 0) + 1 }
     }.toList().first { it.second == 1 }.first
 
