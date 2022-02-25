2d30e5e9e73f394e1c94f3b8de82491cbea6e452
sinoso <tpgus123123@naver.com>
2022-02-25 13:51:18 +0900

    Update le_1528.kt

--- src/leetcode/easy/le_1528.kt
+++ src/leetcode/easy/le_1528.kt
@@ -4,6 +4,10 @@ fun main() {
     println(restoreString("codeleet", intArrayOf(4, 5, 6, 7, 0, 2, 1, 3)))
     println(restoreString("aiohn", intArrayOf(3, 1, 4, 2, 0)))
 }
-private fun restoreString(s: String, indices: IntArray): String =
-        s.withIndex().sortedBy { indices[it.index] }.map { it.value }.joinToString("")
 
+private fun restoreString(s: String, indices: IntArray): String =
+    String(CharArray(indices.size).apply { indices.forEachIndexed { index, i -> this[i] = s[index] } })
+
+
+private fun restoreString2(s: String, indices: IntArray): String =
+    s.withIndex().sortedBy { indices[it.index] }.map { it.value }.joinToString("")
