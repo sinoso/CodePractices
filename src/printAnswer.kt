045daeeea06459fa40ce2ce1efb1fc420c5efee2
sinoso <tpgus123123@naver.com>
2022-02-28 15:02:53 +0900

    Update printAnswer.kt

--- src/printAnswer.kt
+++ src/printAnswer.kt
@@ -1,5 +1,30 @@
+fun <T> printResult(
+    actual: T,
+    expected: T,
+    equalChecker: (actual: T, expected: T) -> Boolean = { it1, it2 -> it1 == it2 },
+) =
+    println(actual.also {
+        if (equalChecker(actual, expected)) print("pass ") else print("fail ")
+        print("expected = $expected , actual = ")
+    })
 
-fun <T> printResult(actual: T, expected: T) =
-    println(actual.also { if (actual == expected) print("pass ") else print("fail ")
-    print("expected = $expected , actual = ")})
+fun <T> printResult(actual: Array<T>, expected: Array<T>) = printResult(actual, expected, ::arrayChecker)
+fun printResult(actual: IntArray, expected: IntArray) = printResult(actual, expected, ::intArrayChecker)
 
+private fun <T> arrayChecker(actual: Array<T>, expected: Array<T>): Boolean {
+    if (actual.size != expected.size)
+        return false
+    for (i in actual.indices)
+        if (actual[i] != expected[i])
+            return false
+    return true
+}
+
+private fun intArrayChecker(actual: IntArray, expected: IntArray): Boolean {
+    if (actual.size != expected.size)
+        return false
+    for (i in actual.indices)
+        if (actual[i] != expected[i])
+            return false
+    return true
+}
