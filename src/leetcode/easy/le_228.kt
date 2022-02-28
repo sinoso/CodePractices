c2059debb374550675a72af3078a3bfeb260d69b
sinoso <tpgus123123@naver.com>
2022-02-28 11:45:04 +0900

    Update le_228.kt

--- src/leetcode/easy/le_228.kt
+++ src/leetcode/easy/le_228.kt
@@ -2,18 +2,46 @@ package leetcode.easy
 
 
 fun main() {
-//    println(summaryRanges(intArrayOf(0, 1, 2, 4, 5, 7)).joinToString())
-//    println(summaryRanges(intArrayOf(0, 1, 2, 4, 6, 7)).joinToString())
-//    println(summaryRanges(intArrayOf(0, 1, 2, 4, 5)).joinToString())
-//    println(summaryRanges(intArrayOf(0, 1, 3)).joinToString())
-//    println(summaryRanges(intArrayOf(0, 2)).joinToString())
-//    println(summaryRanges(intArrayOf(0, 1)).joinToString())
-//    println(summaryRanges(intArrayOf(0)).joinToString())
-//    println(summaryRanges(intArrayOf()).joinToString())
-    println(summaryRanges(intArrayOf(-2147483648,-2147483647,2147483647)).joinToString())
+    println(summaryRanges(intArrayOf(0, 1, 2, 4, 5, 7)).joinToString())
+    println(summaryRanges(intArrayOf(0, 1, 2, 4, 6, 7)).joinToString())
+    println(summaryRanges(intArrayOf(0, 1, 2, 4, 5)).joinToString())
+    println(summaryRanges(intArrayOf(0, 1, 3)).joinToString())
+    println(summaryRanges(intArrayOf(0, 2)).joinToString())
+    println(summaryRanges(intArrayOf(0, 1)).joinToString())
+    println(summaryRanges(intArrayOf(0)).joinToString())
+    println(summaryRanges(intArrayOf()).joinToString())
+    println(summaryRanges(intArrayOf(-2147483648, -2147483647, 2147483647)).joinToString())
 }
 
 private fun summaryRanges(nums: IntArray): List<String> {
+    if (nums.isEmpty())
+        return emptyList()
+    val answer = mutableListOf<String>()
+    var start = nums[0]
+    var last = nums[0]
+    var i = 1
+    while (i < nums.size) {
+        if (last + 1 == nums[i]) {
+            last = nums[i]
+            i++
+            continue
+        }
+        if (last == start)
+            answer.add("$start")
+        else
+            answer.add("$start->$last")
+        start = nums[i]
+        last = nums[i]
+        i++
+    }
+    if (last == start)
+        answer.add("$start")
+    else
+        answer.add("$start->$last")
+    return answer
+}
+
+private fun summaryRanges2(nums: IntArray): List<String> {
     if (nums.isEmpty())
         return emptyList()
     val answer = mutableListOf<String>()
