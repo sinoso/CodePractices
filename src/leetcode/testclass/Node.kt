9e87ddf96ec57c21c2de9a4270edee70c85a8466
sinoso <tpgus123123@naver.com>
2022-03-02 00:25:42 +0900

    Update Node.kt

--- src/leetcode/testclass/Node.kt
+++ src/leetcode/testclass/Node.kt
@@ -2,4 +2,6 @@ package leetcode.testclass
 
 class Node(var `val`:Int) {
     var next: Node? = null
+    var neighbors: ArrayList<Node?> = ArrayList<Node?>()
+    var children: List<Node?> = listOf()
 }
