620a5579cfbde2011a0cf6b1315ea623ea4ec4f9
sinoso <tpgus123123@naver.com>
2022-03-05 10:30:43 +0900

    Update lm_799.kt

--- src/leetcode/medium/lm_799.kt
+++ src/leetcode/medium/lm_799.kt
@@ -10,15 +10,12 @@ private fun champagneTower(poured: Int, query_row: Int, query_glass: Int): Doubl
     if (poured == 0)
         return 0.0
     val glassArray = Array(query_row + 1) { Array(it + 1) { 0.0 } }
-    val accessArray = Array(query_row + 1) { Array(it + 1) { 0 } }
     glassArray[0][0] = poured.toDouble()
     for (i in 0 until glassArray.lastIndex) {
         for (j in glassArray[i].indices) {
             if (glassArray[i][j] > 1.0) {
                 val flood = (glassArray[i][j] - 1) / 2
                 glassArray[i + 1][j] += flood
-                accessArray[i + 1][j]++
-                accessArray[i + 1][j + 1]++
                 glassArray[i + 1][j + 1] += flood
             }
         }
