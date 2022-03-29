df6f854e702b5185c6af008fdbbd13f1c406cdf7
sinoso <tpgus123123@naver.com>
2022-03-29 18:55:53 +0900

    Update printAnswer.kt

--- src/printAnswer.kt
+++ src/printAnswer.kt
@@ -7,6 +7,15 @@ fun <T> printResult(
         if (equalChecker(actual, expected)) print("pass ") else print("fail ")
         print("expected = $expected , actual = ")
     })
+fun  printResult(
+    actual: IntArray,
+    expected: IntArray,
+    equalChecker: (actual: IntArray, expected: IntArray) -> Boolean = { it1, it2 -> it1 == it2 },
+) =
+    println(actual.joinToString(", ").also {
+        if (equalChecker(actual, expected)) print("pass ") else print("fail ")
+        print("expected = ${expected.joinToString(", ")} , actual = ")
+    })
 
 fun <T> printResult(actual: Array<T>, expected: Array<T>) = printResult(actual, expected, ::arrayChecker)
 fun printResult(actual: IntArray, expected: IntArray) = printResult(actual, expected, ::intArrayChecker)
