5ef1b6e39279fd026d9f054fbe4d0791ff7865bd
sinoso <tpgus123123@naver.com>
2022-04-08 13:18:25 +0900

    Update le_703.kt

--- src/leetcode/easy/le_703.kt
+++ src/leetcode/easy/le_703.kt
@@ -1,12 +1,24 @@
 package leetcode.easy
 
+import java.util.*
+
 
 fun main() {
     val kL = KthLargest(3, intArrayOf(4, 5, 8, 2))
     println(kL.add(3))
 }
 
-class KthLargest(k: Int, nums: IntArray) {
+class KthLargest(val k: Int, nums: IntArray) {
+    private val heap = PriorityQueue<Int>().apply { nums.forEach { offer(it) } }
+    fun add(`val`: Int): Int {
+        heap.offer(`val`)
+        while (heap.size > k)
+            heap.poll()
+        return heap.peek()
+    }
+}
+
+class KthLargest2(k: Int, nums: IntArray) {
     private val list = nums.sortedDescending().toMutableList()
     private val kth = k - 1
 
