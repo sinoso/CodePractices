49f320d9c561c4d79a55a97b92e4eba14ee1bf09
sinoso <tpgus123123@naver.com>
2022-04-24 01:32:35 +0900

    Update P2_87390.kt

--- src/programmers/p2/P2_87390.kt
+++ src/programmers/p2/P2_87390.kt
@@ -5,12 +5,9 @@ fun main() {
 }
 
 private fun solution(n: Int, left: Long, right: Long): IntArray {
-    val size = right - left + 1
-    val answer = IntArray(size.toInt())
-    for ((idx, i) in (left..right).withIndex()) {
-        val x = (i % n).toInt()
-        val y = (i / n).toInt()
-        answer[idx] = if (x > y) (x + 1) else (y + 1)
-    } 
-    return answer
+    return (left..right).map {
+        val x = (it % n).toInt()
+        val y = (it / n).toInt()
+        if (x > y) (x + 1) else (y + 1)
+    }.toIntArray()
 }
