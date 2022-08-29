dde4a2d8e92cd84f7e43a60758138408692472bb
sinoso <tpgus123123@naver.com>
2022-08-29 14:50:53 +0900

    Create P118666.kt

---
+++ src/programmers/p1/P118666.kt
@@ -0,0 +1,43 @@
+package programmers.p1
+
+fun main() {
+    solution(arrayOf("AN", "CF", "MJ", "RT", "NA"), intArrayOf(5, 3, 2, 7, 5))
+}
+
+//14:22 ~ 14:48
+private fun solution(survey: Array<String>, choices: IntArray): String =
+    choices.zip(survey).map { (choice, survey) ->
+        val point = choice - 4
+        if (survey[0] - survey[1] < 0) {
+            survey[1] to (point * -1)
+        } else {
+            survey[0] to (point)
+        }
+    }.fold(HashMap<Char, Int>()) { acc, (choice, point) ->
+        acc[choice] = acc.getOrDefault(choice, 0) + point
+        acc
+    }.let { resultMap ->
+        with(StringBuilder()) {
+            if ((resultMap['T'] ?: 0) >= 0) {
+                append('R')
+            } else {
+                append('T')
+            }
+            if ((resultMap['F'] ?: 0) >= 0) {
+                append('C')
+            } else {
+                append('F')
+            }
+            if ((resultMap['M'] ?: 0) >= 0) {
+                append('J')
+            } else {
+                append('M')
+            }
+            if ((resultMap['N'] ?: 0) >= 0) {
+                append('A')
+            } else {
+                append('N')
+            }
+            toString()
+        }
+    }
