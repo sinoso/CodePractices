992a6770e3d4cb7c501159fdf94f453b3abed6b5
sinoso <tpgus123123@naver.com>
2022-08-31 22:36:31 +0900

    Update L811.kt

--- src/leetcode/medium/L811.kt
+++ src/leetcode/medium/L811.kt
@@ -24,5 +24,25 @@ fun subdomainVisits(cpdomains: Array<String>): List<String> {
                 }
             }
     }
-    return resultMap.map { "{${it.value} ${it.value}}" }
+    return resultMap.map { "${it.value} ${it.key}" }
+}
+
+fun subdomainVisits2(cpdomains: Array<String>): List<String> {
+    val resultMap = HashMap<String, Int>()
+    cpdomains.forEach {
+        val spaceIndex = it.indexOf(" ")
+        val req = it.substring(0 until spaceIndex).toInt()
+        var startIndex = spaceIndex + 1
+        val lastIndex = it.lastIndex
+        val firstKey = it.substring(startIndex..lastIndex)
+        resultMap[firstKey] = (resultMap[firstKey] ?: 0) + req
+        while (startIndex < lastIndex) {
+            if (it[startIndex] == '.') {
+                val key = it.substring(startIndex + 1..lastIndex)
+                resultMap[key] = (resultMap[key] ?: 0) + req
+            }
+            startIndex++
+        }
+    }
+    return resultMap.map { "${it.value} ${it.key}" }
 }
