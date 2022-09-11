5eb10ae82221f0aba04d96ca5f170a420921ced8
sinoso <tpgus123123@naver.com>
2022-09-11 15:41:15 +0900

    Update L307.kt

--- src/leetcode/medium/L307.kt
+++ src/leetcode/medium/L307.kt
@@ -1,5 +1,7 @@
 package leetcode.medium
 
+import java.util.Arrays
+
 fun main() {
     NumArray(intArrayOf(1, 3, 5)).apply {
         sumRange(0, 2)
@@ -9,6 +11,67 @@ fun main() {
 }
 
 class NumArray(private val nums: IntArray) {
+    private val segTree = Array(nums.size * 4) { 0 }
+
+    init {
+        buildWithRec(1, 0, nums.size - 1)
+    }
+
+    fun merge(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun buildWithRec(nodeIndex: Int, nodeLeftIndex: Int, nodeRightIndex: Int): Int {
+        if (nodeLeftIndex == nodeRightIndex) {
+            segTree[nodeIndex] = nums[nodeLeftIndex]
+            return segTree[nodeIndex]
+        }
+        val mid = nodeLeftIndex + (nodeRightIndex - nodeLeftIndex) / 2
+        val leftValue = buildWithRec(nodeIndex * 2, nodeLeftIndex, mid)
+        val rightValue = buildWithRec(nodeIndex * 2 + 1, mid + 1, nodeRightIndex)
+        segTree[nodeIndex] = merge(leftValue, rightValue)
+        return segTree[nodeIndex]
+    }
+
+    fun queryRec(left: Int, right: Int, nodeIndex: Int, nodeLeftIndex: Int, nodeRightIndex: Int): Int {
+        if (right < nodeLeftIndex || left > nodeRightIndex) {
+            return 0
+        }
+        if (left <= nodeLeftIndex && right >= nodeRightIndex) {
+            return segTree[nodeIndex]
+        }
+        val mid = nodeLeftIndex + (nodeRightIndex - nodeLeftIndex) / 2
+        val leftValue = queryRec(left, right, nodeIndex * 2, nodeLeftIndex, mid)
+        val rightValue = queryRec(left, right, nodeIndex * 2 + 1, mid + 1, nodeRightIndex)
+        return merge(leftValue, rightValue)
+    }
+
+    fun updateRec(index: Int, newValue: Int, nodeIndex: Int, nodeLeftIndex: Int, nodeRightIndex: Int): Int {
+        if (index < nodeLeftIndex || index > nodeRightIndex) {
+            return segTree[nodeIndex]
+        }
+        if (nodeLeftIndex == nodeRightIndex) {
+            segTree[nodeIndex] = newValue
+            return segTree[nodeIndex]
+        }
+        val mid = nodeLeftIndex + (nodeRightIndex - nodeLeftIndex) / 2
+        val leftValue = updateRec(index, newValue, nodeIndex * 2, nodeLeftIndex, mid)
+        val rightValue = updateRec(index, newValue, nodeIndex * 2 + 1, mid + 1, nodeRightIndex)
+        segTree[nodeIndex] = merge(leftValue, rightValue)
+        return segTree[nodeIndex]
+    }
+
+
+    fun update(index: Int, `val`: Int) {
+        updateRec(index, `val`, 1, 0, nums.size - 1)
+    }
+
+    fun sumRange(left: Int, right: Int): Int {
+        return queryRec(left, right, 1, 0, nums.size - 1)
+    }
+}
+
+class NumArray2(private val nums: IntArray) {
 
     fun update(index: Int, `val`: Int) {
         nums[index] = `val`
@@ -18,7 +81,7 @@ class NumArray(private val nums: IntArray) {
         (left..right).fold(0) { acc, i -> acc + nums[i] }
 }
 
-class NumArray2(private val nums: IntArray) {
+class NumArray3(private val nums: IntArray) {
     private val presumed: IntArray = nums.copyOf()
 
     init {
