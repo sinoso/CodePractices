dd6ef40b53a7feb3b3c8c999eef7825d95e8b795
sinoso <tpgus123123@naver.com>
2022-10-02 14:53:03 +0900

    Create P120818.java

---
+++ src/programmers/p0/P120818.java
@@ -0,0 +1,18 @@
+package programmers.p0;
+
+public class P120818 {
+    public static void main(String[] args) {
+        System.out.println(solution(150000));
+    }
+
+    public static int solution(int price) {
+        if (price < 100_000)
+            return price;
+        if (price < 300_000)
+            return (int) (price * 0.95);
+        if (price < 500_000)
+            return (int) (price * 0.9);
+        return (int) (price * 0.8);
+    }
+
+}
