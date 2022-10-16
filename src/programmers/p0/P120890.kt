d938a52497a9e5e175d67654570f5469954d68db
sinoso <tpgus123123@naver.com>
2022-10-16 09:59:38 +0900

    Update P120890.kt

--- src/programmers/p0/P120890.kt
+++ src/programmers/p0/P120890.kt
@@ -1,10 +1,11 @@
 package programmers.p0
 
+
 fun main() {
 
 }
 
-private fun solution(array: IntArray, n: Int): Int {
-    array.
-    return answer
-}
+private fun solution(array: IntArray, n: Int): Int =
+    array.sortedWith(compareBy<Int> { Math.abs(it - n) }
+        .thenBy { it })
+        .first()
