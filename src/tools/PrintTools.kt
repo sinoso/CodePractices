13629f0652b593d83abcb04ae5987e2e905fc8e9
SeHyeon Kim <tpgus123123@naver.com>
2024-12-07 22:00:37 +0900

    Update PrintTools.kt

--- src/tools/PrintTools.kt
+++ src/tools/PrintTools.kt
@@ -25,3 +25,25 @@ inline fun <T : Any> checkResult(expected: T, actual: T) {
     }
     println(sb)
 }
+
+inline fun  checkResult(expected: Array<String>, actual: Array<String>) {
+    val fileName = Thread.currentThread().stackTrace[1].fileName
+    println(fileName)
+    val sb = StringBuilder()
+    val result = expected.contentEquals(actual)
+    val resultColor = if (result) GREEN_SUCCESS else RED_FAIL
+    val resultText = if (result) "SUCCESS" else "FAIL"
+
+    sb.apply {
+        append("Expected : ")
+        append(expected.joinToString(prefix = "[", separator = ", ", postfix = "]")) //todo Array?[]? [][]? [][][]?
+        append("\nActual   : ")
+        append(actual.joinToString(prefix = "[", separator = ", ", postfix = "]")) //todo Array?[]? [][]? [][][]?
+        append("\nResult   : ")
+        sb.append(resultColor)
+        sb.append(resultText)
+        sb.append(COLOR_EXIT)
+        sb.append("\n")
+    }
+    println(sb)
+}
