5bbf17e3a31c81ac78c55aac4ab7314925252b8d
sinoso <tpgus123123@naver.com>
2026-01-01 17:04:03 +0900

    Update L66.kt

--- src/leetcode/easy/L66.kt
+++ src/leetcode/easy/L66.kt
@@ -23,3 +23,14 @@ private fun plusOne(digits: IntArray): IntArray {
     }
     return digits
 }
+
+private fun plusOne2(digits: IntArray): IntArray {
+    for (i in digits.lastIndex downTo 0) {
+        if (digits[i] != 9) {
+            digits[i]++
+            return digits
+        }
+        digits[i] = 0
+    }
+    return IntArray(digits.size + 1).apply { this[0] = 1 }
+}
